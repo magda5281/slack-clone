2e5f10dcffe318b2fec79592b3d73f894fbce950
Magda <magdagozdalik28@gmail.com>
2022-03-08 11:11:55 +0000

    SidebarOptionChannel Redux implementation

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { db } from "../firebase.js";
 import { collection, addDoc } from "firebase/firestore";
+import { enterRoom } from "../features/appSlice.js";
 
 function SidebarOption({ Icon, title, addChannelOption, id }) {
+  const dispatch = useDispatch();
   const addChannel = () => {
     const channelName = prompt("Enter the channel name");
     if (channelName) {
@@ -18,7 +21,15 @@ function SidebarOption({ Icon, title, addChannelOption, id }) {
         });
     }
   };
-  const selectChannel = () => {};
+  const selectChannel = () => {
+    if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id,
+        })
+      );
+    }
+  };
   return (
     <SidebarOptionContainer onClick={addChannelOption ? addChannel : selectChannel}>
       {Icon && <Icon style={{ margin: 10 }} />}
